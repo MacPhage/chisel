bfe26c0b4a1621d93214c878334174fd13802bb6
FirstWorldAnarchy <tarunbgamer@gmail.com>
2014-12-07 18:09:46 -0500

    Add Debug class, try to run with maven

--- src/main/java/net/austinj/chisel/ui/Chisel.java
+++ src/main/java/net/austinj/chisel/ui/Chisel.java
@@ -1,46 +1,33 @@
 package net.austinj.chisel.ui;
 
 
-import java.net.URL;
-
 import javafx.application.Application;
-import static javafx.application.Application.launch;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import net.austinj.chisel.Debug;
 
-public class Chisel extends Application 
-{
+public class Chisel extends Application {
 
-	@Override
-	public void start(Stage stage) throws Exception 
-	{
-		try
-		{
-			String url = Chisel.class.getResource("/html/index.html").toExternalForm();
-			System.out.println(url);
-			WebView web = new WebView();
-			web.setContextMenuEnabled(false);
-	        web.getEngine().load(url);  
-	        Scene scene = new Scene(web,375,667);
-	        stage.setTitle("Chisel");
-	        stage.setScene(scene);  
-	        stage.show();
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-		
-	}
-	
-	public static void main(String[] args)
-	{
-		launch(args);
-	}
-	
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        String url = getClass().getResource("/html/index.html").toExternalForm();
+        Debug.printInfo(url);
+
+        WebView web = new WebView();
+        web.setContextMenuEnabled(false);
+        web.getEngine().load(url);
+
+        Scene scene = new Scene(web, 375, 667);
+
+        stage.setTitle("Chisel");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }

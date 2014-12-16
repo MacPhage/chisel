1c940e2ac44e9b1b39ccc61bf45b9bf961c8d135
Austin Jackson <themacphage@gmail.com>
2014-12-16 17:04:49 -0600

    Working, pretty node-webkit demo

---
+++ source/app.nw/js/core.js
@@ -0,0 +1,10 @@
+//Important global code here
+
+var gui = require("nw.gui");
+var win = gui.Window.get();
+
+// win.on("minimize",function(){
+//     console.log("Window is minimized!");
+// });
+
+win.resizeTo(375,667);

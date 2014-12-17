f4b41251d4ff74f684d3c8dc43fca372e877fc15
Austin Jackson <theaustinjackson@gmail.com>
2014-12-17 17:06:50 -0600

    Removed node_modules, deps use nom now

--- source/app.nw/main.js
+++ source/app.nw/main.js
@@ -1,1 +1,10 @@
-//Do nothing
+var logger = require("js/logging.js");
+
+logger.startup();
+logger.log("info","Loading web tools...");
+
+var less = require("less");
+
+less.render(".class { width: (1 + 1) }", function (e, output) {
+    console.log(output.css);
+});
